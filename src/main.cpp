727f29df2c8fd55bf29541511f4207b02c1fe207
Srm-Akla <akellamahateja@gmail.com>
2020-12-06 19:16:07 +0100

    adding add, sub

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+int add(int a, int b){
+    return a+b;
+}
+
+int sub(int a, int b){
+    return a-b;
+}
+
 // main() is where program execution begins.
 int main() {
-    std::cout << "Hello World"; // prints Hello World
-   return 0;
+    int a,b;
+    std::string func;
+    
+    std::cout << "Enter function: add, sub, multi, div - ";
+    getline(std::cin, func);
+    std::cout << "Enter num1 - "; // prints Hello World
+    std::cin >> a;
+    std::cout << "Enter num2 - ";
+    std::cin >> b;
+
+    if(func=="add"){
+	std::cout << add(a,b) << std::endl;
+    }
+    else if(func=="sub"){
+	std::cout << sub(a,b) << std::endl;
+    }
+    else{ std::cout << "Not working"; } 
+
+    return 0;
 }
